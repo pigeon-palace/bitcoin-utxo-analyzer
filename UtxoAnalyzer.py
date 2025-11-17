e090f7525cdb4f72f230112d67e98e8454900aa0
pigeon-palace <139773809+pigeon-palace@users.noreply.github.com>
2025-11-17 10:19:33 -0600

    fixes

--- UtxoAnalyzer.py
+++ UtxoAnalyzer.py
@@ -4,6 +4,7 @@ import time
 import os
 import csv
 import sqlite3
+from datetime import datetime
 
 GENESIS_HASH = "80ca095ed10b02e53d769eb6eaf92cd04e9e0759e5be4a8477b42911ba49c78f"
 REST_URL = 'http://127.0.0.1:9332/rest'
@@ -32,8 +33,8 @@ class BlockParser:
             insert_id = tx['txid'] + "_" + str(index)
             try:
                 address = self.script_handler.get_address_from_script(vout['scriptPubKey'])
-            except:
-                raise Exception("script['type'] unknown", tx)
+            except SyntaxError:
+                raise SyntaxError("script['type'] unknown", tx)
             amount = int(vout['value'] * COIN)
             if amount > 0:
                 self.insert_queue.append((insert_id, address, amount, timestamp))
@@ -44,7 +45,7 @@ class BlockParser:
         self.delete_queue = []
         
 class LitecoinScriptHandler:
-    def get_address_from_script(script):
+    def get_address_from_script(self, script):
         if 'addresses' in script:
             address = script['addresses'][0]
         elif script['type'] == 'pubkey':
@@ -58,27 +59,27 @@ class LitecoinScriptHandler:
         elif script['type'] == 'witness_mweb_pegin':
             address = "hogwarts"
         else:
-            raise Exception("script['type'] unknown", script['type'])
+            raise SyntaxError("script['type'] unknown", script['type'])
         return address
        
 class StatWriter:        
-    def compute_address_distribution(self, date, db):
+    def compute_address_sum_by_amount_distribution(self, date, db):
         results = [0 for i in range(16)]
         for row in db.cur.execute("""SELECT SUM(amount) FROM utxo GROUP BY address;"""):
             amount = row[0]
             if amount:
                 results[int(math.log10(amount))] += amount
         row = [date.strftime('%Y-%m-%d')] + results
-        self.append(ADDRESSES_NAME, row)
+        self.append(ADDRESSES_FILENAME, row)
         
-    def compute_age_distribution(self, date, db):
+    def compute_utxo_sum_by_age_distribution(self, date, db):
         results = [0 for i in range(16)]
         for row in db.cur.execute("""SELECT timestamp, amount FROM utxo;"""):
             utxo_timestamp = row[0]
             days_since = max((date.timestamp() - utxo_timestamp)/86400,1)
             results[int(math.log2(days_since))] += row[1]
         row = [date.strftime('%Y-%m-%d')] + results
-        self.append(AGE_NAME, row)
+        self.append(AGE_FILENAME, row)
         
     def append(self, filename, results):
         with open(filename, 'a', newline='') as f:
@@ -105,7 +106,7 @@ class Timer:
     def start(self, tag):
         self.time_lookup[tag] = time.time()
         
-    def finish(self, tag):
+    def end(self, tag):
         duration = time.time() - self.time_lookup[tag]
         print(tag, duration)
         del self.time_lookup[tag]
@@ -155,29 +156,49 @@ def run_from_block(start_height = 0, start_timestamp = None, start_block_hash = 
         day = datetime.utcfromtimestamp(block['time'])
         block_hash = block['nextblockhash']
     
-    for i in range(start_height,10**7):
+    while 'nextblockhash' in block:
         block = parser.getblock(block_hash)
         block_day = datetime.utcfromtimestamp(block['time'])
         if block_day.day != day.day:
-            timer.start("db_insert" + str(day) + "_" + str(i))
+            timer.start("db_insert_" + str(day) + "_" + str(i))
             db.insert(parser.insert_queue)
-            timer.finish("db_insert" + str(day) + "_" + str(i))
-            timer.start("db_delete" + str(day) + "_" + str(i))
+            timer.end("db_insert_" + str(day) + "_" + str(i))
+            timer.start("db_delete_" + str(day) + "_" + str(i))
             db.delete(parser.delete_queue)
             parser.clear()
-            timer.finish("db_delete" + str(day) + "_" + str(i))
+            timer.end("db_delete_" + str(day) + "_" + str(i))
             timer.start("utxo_age_" + str(day) + "_" + str(i))
-            stat.compute_age(day, db)
-            timer.finish("utxo_age_" + str(day) + "_" + str(i))
+            stat.compute_utxo_sum_by_age_distribution(day, db)
+            timer.end("utxo_age_" + str(day) + "_" + str(i))
             timer.start("utxo_address_" + str(day) + "_" + str(i))
-            stat.compute_address(day, db)
-            timer.finish("utxo_address_" + str(day) + "_" + str(i))
+            stat.compute_address_sum_by_amount_distribution(day, db)
+            timer.end("utxo_address_" + str(day) + "_" + str(i))
             day = block_day
-        print(i)
+        print(block['height'])
         for tx in block['tx']:
             parser.process_tx(tx, block['time'])
         block_hash = block['nextblockhash']
         
 if __name__ == "__main__":
-    run_from_block()
+    run_from_block(start_block_hash = "02f64c649898fcb4b79d814504e6d086522350b737f3099d7105c8df1c7e6aa8")
         
+#print last block height somewhere in build.
+#3004797
+#utxo count
+#utxo count distribute by amount
+#utxo count distribute by age
+#utxo sum distribute by amount
+#utxo sum distribute by age
+#utxo sum = supply
+#address count
+#address count distribute by balance
+#address count distribute by age
+#address count distribute by tx_count
+#address sum = supply
+#address sum distribute by age
+#address sum distribute by amount
+#address sum distribute by tx_count
+#tx count
+#tx count by amount
+#tx count by age
+
